3721d3e2c9899d19282eccfe80652b714d1a81f2
agnikki <68161276+agnikki@users.noreply.github.com>
2020-09-05 20:56:48 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,87 @@
+//Create variables here
+var dog,dog_img,happyDog,database,foodStock,foodS=0;
+var foodObj,feedDog,addFood,fedTime,lastFed;
+function preload()
+{
+  dog_img=loadImage("Dog.png");
+  happyDog=loadImage("happydog.png");
+}
+
+function setup() {
+  database = firebase.database();
+	createCanvas(500, 500);
+  dog=createSprite(200,300,10,10);
+  dog.addImage(dog_img);
+  dog.scale=0.1;
+
+  foodStock=database.ref('food');
+  foodStock.on("value",(data)=>{
+    foodS=data.val();
+  });
+  //function to update food stock and last fed time
+  function feedDog(){
+    dog.addImage(happyDog);
+
+    foodObj.updateFookStock(FoodObj.getFoodStock()-1);
+    database.ref('/').update({
+      Food:foodObj.getFoodStock(),
+      FeedTime:hour()
+    })
+  }
+  //function to add food in stock
+  function addFoods(){
+    foodS++;
+    database.ref('/').update({
+      Food:foodS
+    })
+  }
+}
+
+
+function draw() {  
+background(46,139,87);
+
+Food.display();
+feedTime=database.ref('FeedTime');
+fedTime.on("value",function(data){
+ lastFed=data.val();
+});
+
+
+  drawSprites();
+  //add styles here
+  stroke("black");
+  textSize(20);
+ text("foodStock:"+ foodS, 200,100);
+ 
+ text("Note:Press up arrow key to feed dog milk!",100,50);
+
+}
+function writeStock(x){
+  if(x<=0){
+    x=0
+  }
+  else{x=x-1}
+  database.ref('/').update({
+    food:x
+  })
+}
+function FeedDog(){
+feed=createButton("Feed the dog");
+feed.position(700,95);
+feed.mousePressed(FeedDog);
+feed.button.Mousepressed(()=>{
+    feed.button.hour()
+    feed.writeStock(foodS);
+    dog.addImage(happyDog);
+}) 
+}
+
+function addFood(){
+  addFood=createButton("Add Food");
+  addFood.position(800,95);
+  addFood.mousePressed(addFoods+1);
+}
+
+
+
